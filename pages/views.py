30c08989d3f5922f49794dfee8551bd8b384ba19
himalg7 <thapahimal777@gmail.com>
2023-01-26 15:33:10 +0545

    first commit

---
+++ pages/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+import openai
+from dotenv import load_dotenv
+import os
+#python manage.py runserver
+
+def home(request):
+    return render(request, 'pages/home.html')
+
+def essay_writing(request):
+    if request.method == 'POST':
+        prompt = request.POST['question']
+        language = request.POST['language']
+        prompt = "Write an essay in "+language+" about "+prompt
+        print(prompt)
+        load_dotenv()
+        openai.api_key = os.environ.get("GPT3_KEY")
+        # openai.api_key = "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"
+        response = openai.Completion.create(
+            model="text-davinci-002",
+            prompt=prompt,
+            max_tokens=200,
+        )
+        answer = response['choices'][0]['text']
+        print(answer)
+    else:
+        answer = None
+    return render(request, 'pages/essay_writing.html', {'answer': answer})
+
+def paraphrase(request):
+    if request.method == 'POST':
+        prompt = request.POST['question']
+        language = request.POST['language']
+        prompt = "Paraphrase in "+language+" this "+prompt
+        print(prompt)
+        load_dotenv()
+        openai.api_key = os.environ.get("GPT3_KEY")
+        response = openai.Completion.create(
+            model="text-davinci-002",
+            prompt=prompt,
+            max_tokens=200,
+        )
+        answer = response['choices'][0]['text']
+        print(answer)
+    else:
+        answer = None
+    return render(request, 'pages/paraphrase.html', {'answer': answer})
+
+def about(request):
+    return render(request, 'pages/about.html')
